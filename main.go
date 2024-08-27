864eb74e4ad996ad2e6422754e37a766914878f7
Rebel028 <27502053+Rebel028@users.noreply.github.com>
2024-08-27 14:08:07 +0400

    [fea] added error logging to file

--- main.go
+++ main.go
@@ -5,18 +5,32 @@ import (
 	"github.com/pdfcpu/pdfcpu/pkg/api"
 	"github.com/pdfcpu/pdfcpu/pkg/pdfcpu"
 	"github.com/pdfcpu/pdfcpu/pkg/pdfcpu/model"
+	"io"
 	"io/fs"
 	"log"
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
 )
 
 const inDir string = "." //todo: handle input
+var logName = fmt.Sprintf("errors%s.log", time.Now().Format("20060102150405"))
 
 var conf *model.Configuration = model.NewDefaultConfiguration()
 
 func main() {
+
+	//configure logging
+	logFile, err := os.OpenFile(logName, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
+	if err != nil {
+		panic(err)
+	}
+	mw := io.MultiWriter(os.Stdout, logFile)
+	log.SetOutput(mw)
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+
+	//let's go
 	dirAbsPath, _ := filepath.Abs(inDir)
 	fmt.Printf("Checking files in %s\n"+
 		"Make sure you have backed up your files (just in case)\n"+
@@ -26,7 +40,7 @@ func main() {
 		log.Panic(err)
 	}
 
-	err := filepath.WalkDir(inDir, func(path string, f fs.DirEntry, err error) error {
+	err = filepath.WalkDir(inDir, func(path string, f fs.DirEntry, err error) error {
 		if err != nil {
 			// handle possible path err, just in case...
 			return err
@@ -44,12 +58,29 @@ func main() {
 	}
 
 	defer func() {
+		logFile.Close()
 		fmt.Println("Done! Press Enter to exit")
 		//wait for user input
 		if _, err = fmt.Scanln(); err != nil {
 			log.Panic(err)
 		}
+		deleteEmptyErrorLog(logName)
 	}()
+}
+
+func deleteEmptyErrorLog(fileName string) {
+	fi, err := os.Stat(fileName)
+	if err != nil {
+		log.Panic(err)
+	}
+	// get the size
+	size := fi.Size()
+	if size == 0 {
+		err := os.Remove(fileName)
+		if err != nil {
+			log.Panic(err)
+		}
+	}
 }
 
 func fixPdf(inFile string) {
